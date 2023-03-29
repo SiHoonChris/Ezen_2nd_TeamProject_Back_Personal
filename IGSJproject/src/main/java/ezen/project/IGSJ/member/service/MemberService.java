5e026acad80b21fcc0845c025bf0d3800f17bfad
SiHoonChris <chrisleegpock@gmail.com>
2023-03-29 14:10:54 +0900

    2nd commit 03.29 (comments on commit 참고)

--- IGSJproject/src/main/java/ezen/project/IGSJ/member/service/MemberService.java
+++ IGSJproject/src/main/java/ezen/project/IGSJ/member/service/MemberService.java
@@ -4,12 +4,8 @@ import ezen.project.IGSJ.member.domain.MemberDTO;
 
 public interface MemberService {
 
-	//회원가입 로직
-	public void signUpMember(MemberDTO memberDTO) throws Exception;
-	
 	//로그인 기능
 	public MemberDTO memberLogin(MemberDTO memberDTO) throws Exception;
 	
-	// 회원가입 아이디 중복 체크
-	public int memberIdCheck(MemberDTO memberDTO) throws Exception;
-}
+	
+} // public interface MemberService
